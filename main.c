83fcc010da447be39872eed8f11eb168937a429e
Tom Liang <tomliangg@gmail.com>
2019-01-23 10:55:08 -0800

    comment update for lab1 activity 1

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
+/*
+ * Author: Tom Liang
+ * Date: Jan 23, 2019
+ * Objective: read the state of SW1, and switch the Blue LED on if SW1 is closed
+ * and switch it off if SW1 is open
+ */
+
+
 #include "tm4c123gh6pm.h"
 #include <stdint.h>
 #include <stdbool.h>
 
 void PortF_Init(void) {
-    SYSCTL_RCGC2_R |= 0x00000020; // F clock
-    while ((SYSCTL_PRGPIO_R & 0x00000020) == 0) {}; // delay
+    SYSCTL_RCGC2_R |= 0x00000020; // activate clock for PortF
+    while ((SYSCTL_PRGPIO_R & 0x00000020) == 0) {}; // wait until PortF is ready
     GPIO_PORTF_LOCK_R = 0x4C4F434B; // unlock PortF PF0; unlocking is needed only for pins PC3-0, PD7, PF0
     GPIO_PORTF_CR_R = 0x1F; // allow changes to PF4-0
     GPIO_PORTF_AMSEL_R = 0x00; // disable analog function
@@ -23,10 +31,10 @@ int main(void)
     PortF_Init();
     while (1) {
         SW1 = GPIO_PORTF_DATA_R & 0x10; // read PF4 into SW1
-        SW1 = SW1 >> 2; // 0x10 >> 2 = 0x04
+        SW1 = SW1 >> 2; // 0x10 >> 2 = 0x04, shift into PF2 position
         Out = GPIO_PORTF_DATA_R;
-        Out = Out & 0xF1;
-        Out = Out | SW1;
+        Out = Out & 0xFB; // clear bit at PF2 position
+        Out = Out | SW1; // set bit according to SW1
         GPIO_PORTF_DATA_R = Out;
     }
 	return 0;
