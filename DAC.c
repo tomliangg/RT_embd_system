9579db27d8d511db0d8ea73c52b26055f859a12b
Tom Liang <tomliangg@gmail.com>
2019-03-16 17:40:00 -0700

    use tab for spacing

--- DAC.c
+++ DAC.c
@@ -9,14 +9,14 @@
 // Initialize 4-bit DAC on PortB
 void DAC_Init(void) {
     unsigned long volatile delay;
-	SYSCTL_RCGC2_R |= 0x02;            // activate port B
-	delay = SYSCTL_RCGC2_R;            // allow time to finish activating
-	GPIO_PORTB_AMSEL_R &= ~0x0F;       // no analog
-	GPIO_PORTB_PCTL_R &= ~0x0000FFFF;  // regular GPIO function
-	GPIO_PORTB_DIR_R |= 0x0F;          // make PB3-0 out
-	GPIO_PORTB_DR8R_R |= 0x0F;         // can drive up to 8mA out
-	GPIO_PORTB_AFSEL_R &= ~0x0F;       // disable alt funct on PB3-0
-	GPIO_PORTB_DEN_R |= 0x0F;          // enable digital I/O on PB3-0
+    SYSCTL_RCGC2_R |= 0x02;            // activate port B
+    delay = SYSCTL_RCGC2_R;            // allow time to finish activating
+    GPIO_PORTB_AMSEL_R &= ~0x0F;       // no analog
+    GPIO_PORTB_PCTL_R &= ~0x0000FFFF;  // regular GPIO function
+    GPIO_PORTB_DIR_R |= 0x0F;          // make PB3-0 out
+    GPIO_PORTB_DR8R_R |= 0x0F;         // can drive up to 8mA out
+    GPIO_PORTB_AFSEL_R &= ~0x0F;       // disable alt funct on PB3-0
+    GPIO_PORTB_DEN_R |= 0x0F;          // enable digital I/O on PB3-0
 }
 
 
@@ -24,5 +24,5 @@ void DAC_Init(void) {
 // output to DAC
 // Input: 4-bit data, 0 to 15 
 void DAC_Out(unsigned long data) {
-	GPIO_PORTB_DATA_R = data;
+    GPIO_PORTB_DATA_R = data;
 }
--- Sound.c
+++ Sound.c
@@ -13,12 +13,12 @@ unsigned char SW_prev;  // previous switch values for portE
 // **************Sound_Init*********************
 // Initialize Systick periodic interrupts
 void Sound_Init(void) {
-	index = 0;
-	NVIC_ST_CTRL_R = 0;  // disable SysTick during setup
+    index = 0;
+    NVIC_ST_CTRL_R = 0;  // disable SysTick during setup
     NVIC_ST_RELOAD_R = period-1;  // reload value
-	NVIC_ST_CURRENT_R = 0;  // any write to current clears it
-	NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x00FFFFFF)|0x20000000;  //set priority to 1
-	NVIC_ST_CTRL_R = 0x0007;  // enable,core clock, and interrupts
+    NVIC_ST_CURRENT_R = 0;  // any write to current clears it
+    NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x00FFFFFF)|0x20000000;  //set priority to 1
+    NVIC_ST_CTRL_R = 0x0007;  // enable,core clock, and interrupts
 }
 
 // **************Sound_Tone*********************
@@ -29,7 +29,7 @@ void Sound_Init(void) {
 //           Minimum is determined by length of ISR
 void Sound_Tone(unsigned long period_local) {
 // this routine sets the RELOAD and starts SysTick
-	period = period_local;
+    period = period_local;
 }
 
 
@@ -37,25 +37,25 @@ void Sound_Tone(unsigned long period_local) {
 // stop outputing to DAC
 void Sound_Off(void) {
  // this routine stops the sound output
-	GPIO_PORTB_DATA_R &= ~0x0F;
+    GPIO_PORTB_DATA_R &= ~0x0F;
 }
 
 
 // Interrupt service routine
 void SysTick_Handler(void) {
-	SW_current = GPIO_PORTE_DATA_R & 0x0F;  // read PortE into SW_current
-	if ((SW_current != SW_prev) && SW_current) {
-		if (GPIO_PORTE_DATA_R & 0x01) Sound_Tone(80000000/(16*523.251));  // set note C pitch
-		if (GPIO_PORTE_DATA_R & 0x02) Sound_Tone(80000000/(16*587.330));  // set note D pitch
-		if (GPIO_PORTE_DATA_R & 0x04) Sound_Tone(80000000/(16*659.255));  // set note E pitch
-		if (GPIO_PORTE_DATA_R & 0x08) Sound_Tone(80000000/(16*783.991));  // set note G pitch
-		Sound_Init();  //load the new frequency to ISR
-	}
+    SW_current = GPIO_PORTE_DATA_R & 0x0F;  // read PortE into SW_current
+    if ((SW_current != SW_prev) && SW_current) {
+        if (GPIO_PORTE_DATA_R & 0x01) Sound_Tone(80000000/(16*523.251));  // set note C pitch
+        if (GPIO_PORTE_DATA_R & 0x02) Sound_Tone(80000000/(16*587.330));  // set note D pitch
+        if (GPIO_PORTE_DATA_R & 0x04) Sound_Tone(80000000/(16*659.255));  // set note E pitch
+        if (GPIO_PORTE_DATA_R & 0x08) Sound_Tone(80000000/(16*783.991));  // set note G pitch
+        Sound_Init();  //load the new frequency to ISR
+    }
 
-	if (SW_current) {
-	    index = (index + 1) & 0x0F;  // 4,5,6,7,8,9,10,11,12,11,10,9,8,7,6,5...�
+    if (SW_current) {
+        index = (index + 1) & 0x0F;  // 4,5,6,7,8,9,10,11,12,11,10,9,8,7,6,5...�
         DAC_Out(SineWave[index]);  // output one value at each interrupt
-	} else if (!SW_current) Sound_Off();
+    } else if (!SW_current) Sound_Off();
 
-	SW_prev = SW_current;
+    SW_prev = SW_current;
 }
